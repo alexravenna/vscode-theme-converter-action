608f6b4b110bc8c223c7c62f6efd37ca7c51fc95
alexravenna <alex.n.ravenna@gmail.com>
2023-10-15 13:03:32 +0200

    start building dotnet project in container

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/sh -l
+#!/bin/sh
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+git clone https://github.com/microsoft/theme-converter-for-vs
+
+cp theme.json theme-converter-for-vs/ThemeConverter/ThemeConverter
+
+cd theme-converter-for-vs/ThemeConverter/ThemeConverter
+dotnet dotnet build ThemeConverter.csproj
+
+cd bin/Debug/net6.0
