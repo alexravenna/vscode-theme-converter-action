976fcaba9faa2e17546dae073c5a294a67b3256e
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-03-07 04:53:46 +0000

    test template search

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ dotnet dotnet build ThemeConverter.csproj
 cd bin/Debug/net6.0
 
 # Install template for VSIX project and
-dotnet new install Microsoft.VisualStudio.Sdk.Templates
+dotnet new search vsix
+# dotnet new install Microsoft.VisualStudio.Sdk.Templates
 
 # Create new empty VSIX project
-dotnet new vsix
+# dotnet new vsix
