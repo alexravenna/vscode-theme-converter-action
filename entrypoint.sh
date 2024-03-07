1e46e84498a3d4c866e6a6cf478ca2d82cf92489
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-03-07 04:52:37 +0000

    add creation of empty VSIX project

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,3 +8,9 @@ cd theme-converter-for-vs/ThemeConverter/ThemeConverter
 dotnet dotnet build ThemeConverter.csproj
 
 cd bin/Debug/net6.0
+
+# Install template for VSIX project and
+dotnet new install Microsoft.VisualStudio.Sdk.Templates
+
+# Create new empty VSIX project
+dotnet new vsix
