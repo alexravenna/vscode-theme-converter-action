24701f8bf28382eedfff6a857ab1b4c740de0e84
Alex Ravenna <alex.n.ravenna@gmail.com>
2024-03-07 05:06:16 +0000

    move template installation to top in order to short-circuit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Install template for VSIX project
+dotnet new install Microsoft.VisualStudio.Sdk.Templates
+
 git clone https://github.com/microsoft/theme-converter-for-vs
 
 cp theme.json theme-converter-for-vs/ThemeConverter/ThemeConverter
@@ -8,9 +11,6 @@ cd theme-converter-for-vs/ThemeConverter/ThemeConverter
 dotnet dotnet build ThemeConverter.csproj
 
 cd bin/Debug/net6.0
-
-# Install template for VSIX project and
-dotnet new install Microsoft.VisualStudio.Sdk.Templates
 
 # Create new empty VSIX project
 dotnet new vsix
